fd2523c3ff3e05249d6160129cf706f08b08837a
Jesus Sanchez <geluxedeluxe@gmail.com>
2018-09-28 14:03:35 +0200

    Ng repeat 2

--- Ejercicio - Repeticiones/js/app.js
+++ Ejercicio - Repeticiones/js/app.js
@@ -6,7 +6,22 @@ app.controller('listadoCtrl', ['$scope', function($scope){
 	
     $scope.listado = ["Jesus", "yo", "tu", "el", "nosotros"];
 
-
+    $scope.listadoProfesores = {
+        profesores:[{
+            nombre: "Jesus",
+            edad: 123,
+            clase: "COSA"
+        },{
+            nombre: "Jesus2",
+            edad: 434,
+            clase: "COSA2"
+        },{
+            nombre: "Jesus3",
+            edad: 8765,
+            clase: "PDD"
+        }
+    ]
+    }
 
 }]);
 
