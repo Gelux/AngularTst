8faeb7a695f599e3d84f3d5ff47fc5d7822ed039
Jesus Sanchez <geluxedeluxe@gmail.com>
2018-09-28 14:52:15 +0200

    Cambios pre AJAXCambios pre AJAX

--- Ejercicio - http request/js/app.js
+++ Ejercicio - http request/js/app.js
@@ -6,7 +6,12 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
-  
+    $scope.profesores={};
+
+    $http.get('json/profesores.json').success(function(data){
+        //Si la petición  es correcta
+        $scope.profesores=data.profesores;
+    });
 
 
   
