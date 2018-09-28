a2f22d3e622364d5266964870ed9ff8b36a028d1
Jesus Sanchez <geluxedeluxe@gmail.com>
2018-09-28 13:42:44 +0200

    Ejer2 completo

--- Ejercicio 02 - Vacio/js/app.js
+++ Ejercicio 02 - Vacio/js/app.js
@@ -3,24 +3,36 @@ var app = angular.module('collegeApp', []);
 app.controller('prfCtrl', function($scope){
     $scope.profesor = profesorData;
     $scope.editando = {};
-
+    
+    $scope.mostrarCaja = false;
+    
     $scope.EditarProfesor = function(){
 
         angular.copy( $scope.profesor, $scope.editando );
+        $scope.mostrarCaja = true;
 
     }
-
 
     $scope.GuardarCambios= function(){
 
         angular.copy( $scope.editando, $scope.profesor );
+
+        $scope.mostrarCaja = false;
+
+    }
+
+    $scope.CancelarCambios= function(){
+
+        $scope.editando = {};
+
+        $scope.mostrarCaja = false;
 
     }
 });
 
 
 var profesorData = {
-    nombre: "nombre",
+    nombre: "nombre N.",
     bio: "holaquetal",
     edad: 77,
     foto: "img/icono.png"
